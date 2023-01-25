37e06499b5832c3ebf7e09fdd45be5b0b16300b6
Jsegur <92443084+Jsegur@users.noreply.github.com>
2023-01-25 17:01:51 +0100

    Add files via upload

---
+++ ProjetCsharp/Stat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetCsharp.Combat
+{
+    internal class Statut
+    {
+        public enum stat
+        {
+            Health,
+            Strenght,
+
+        }
+
+
+
+    }
+}
