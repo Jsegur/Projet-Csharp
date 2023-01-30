4b40810d2e15389f32da39a6ea902b1d36da2f4b
Jsegur <92443084+Jsegur@users.noreply.github.com>
2023-01-30 11:49:50 +0100

    Add files via upload

---
+++ Level.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetCsharp.Combat
+{
+    internal class Level
+    {
+        int _level;
+        bool levelUp = false;
+        int _exp = 0;
+        int _addExp = 100;
+        int _expMax = 100;
+        int _point;
+
+        public int Level_ { get => _level; set => _level = value; }
+        public int GetLevel()
+        {
+            return _level;
+        }
+        public void SetLevel(int value)
+        {
+            _level = value;
+        }
+
+        private void LevelUp()
+        {
+            while (kill = true)
+            {
+                if (_exp = _expMax)
+
+            }
+
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+enum PlayerState
+{
+    IDLE = 0,
+    WALK = 1,
+    RUN = 2,
+    SWIM = 3,
+}
+
+namespace ProjetCsharp
+{
+    internal class test
+    {
+        void main()
+        {
+
+            Console.WriteLine("Hello, World!");
+
+            int a;
+            string aa;
+            float aaa;
+            double aaaa;
+
+            var _age = 15; //champ
+
+            var age = 12;
+            (string, int, float, double, bool) b = ("", 12, 2.5f, 2.5, true);
+            var bb = ("", 12, 2.5f, 2.5, true);
+
+            string name = "hello";
+
+            Console.WriteLine(age < 18 ? "mineur" : "majeur");
+
+            PlayerState monState = PlayerState.IDLE;
+            switch (monState)
+            {
+                case PlayerState.IDLE:
+                    break;
+                case PlayerState.WALK:
+                    Console.WriteLine("1");
+                    break;
+                case PlayerState.RUN:
+                    Console.WriteLine("2");
+                    break;
+                case PlayerState.SWIM:
+                    break;
+                default:
+                    break;
+            }
+
+            dynamic yo = 12;
+            yo = "cc";
+            yo = false;
+
+            dynamic CoUcOu(dynamic para)
+            {
+                return 0;
+            }
+
+            foreach (var val in new[] { 12, 13, 14, 15 })
+            {
+                foreach (var val2 in new[] { 12, 13, 14, 15 })
+                {
+
+                }
+                if (val % 2 == 0) break;
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
+
